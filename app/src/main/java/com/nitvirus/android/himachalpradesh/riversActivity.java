f2e1a7d53fa4e623322d9ece172a23d88007039f
nitvirus <er.nitishguleria@gmail.com>
2017-10-10 00:21:57 +0530

    small chnges

--- app/src/main/java/com/nitvirus/android/himachalpradesh/riversActivity.java
+++ app/src/main/java/com/nitvirus/android/himachalpradesh/riversActivity.java
@@ -1,20 +1,18 @@
 package com.nitvirus.android.himachalpradesh;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class detailedActivity extends AppCompatActivity {
+public class riversActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailed);
+        setContentView(R.layout.activity_rivers);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,16 +24,6 @@ public class detailedActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        Button readrivers = (Button)findViewById(R.id.readrivers);
-        readrivers.setOnClickListener(
-                new Button.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(new Intent(detailedActivity.this, riversActivity.class));
-                    }
-
-                }
-        );
     }
 
 }
